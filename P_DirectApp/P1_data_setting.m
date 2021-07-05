ac1fbb52321678330ccdd4f68a0bb4f743547768
NoboruSakamoto <noboru.sakamoto@nanzan-u.ac.jp>
2021-07-05 17:05:05 +0900

    Diagonalize^{-1} causes a lot of warnings when its condition number is bad. The inverse is given using simplectic inverse formula.

--- P_DirectApp/P1_data_setting.m
+++ P_DirectApp/P1_data_setting.m
@@ -22,7 +22,8 @@ L = lyap(A - Rbar*P,-Rbar);
 %資料ではW
 Diagonalize = [ eye(dim), L            ;
     P,        P*L+eye(dim)];
-Diag_inv = Diagonalize^(-1);
+Diag_inv = [eye(dim)+L*P, -L; 
+                -P, eye(dim)];  %Diagonalize^(-1); changed using simplectic property
 Ham = [A,-Rbar;-2*Q,-A'];
 %% === 各種行列表示 ===
 fprintf('\n')
